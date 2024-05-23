1160f7a8b828200b4e533485ab1c87261f4974fa
SaulRivera99 <saulrivera9999@gmail.com>
2024-05-23 10:45:54 -0600

    Arreglando redireccionamiento a dashboard segun rol del usuario

--- src/router.tsx
+++ src/router.tsx
@@ -123,6 +123,176 @@
 
 // export default App;
 
+// import React, { useEffect, useState, ReactElement } from "react";
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Routes,
+//   Navigate,
+//   Outlet,
+// } from "react-router-dom";
+// import { Navbar } from "./components/Navbar";
+// import { Register } from "./views/Register";
+// import { Grade } from "./views/Grade";
+// import { Subject } from "./views/Subject";
+// import { Students } from "./views/Students";
+// import { Attendance } from "./views/Attendance";
+// import { Teacher } from "./views/Teacher";
+// import { Observation } from "./views/Observations";
+// import { MiGrade } from "./views/MiGrade";
+// import { ReportGrade } from "./views/ReportGrade";
+// import { Rols } from "./views/Rols";
+// import LoginForm from "./views/Login/Login";
+// import { Parents } from "./views/Parents";
+// import { ParentAssociations } from "./views/ParentAssociations";
+// import { Toaster } from "react-hot-toast";
+// import { GradeForm, GradeList, GradeEdit } from "./views/Grade";
+// import { StudentsGrade } from "./views/StudentsGrade";
+// import { AdminDashboard, ProfessorDashboard } from "./views/Dashboard";
+
+// const App = () => {
+//   const [isAuthenticated, setIsAuthenticated] = useState(
+//     !!localStorage.getItem("token")
+//   );
+
+//   useEffect(() => {
+//     const token = localStorage.getItem("token");
+//     if (token) {
+//       setIsAuthenticated(true);
+//     }
+//   }, []);
+
+//   const AuthGuard = () => {
+//     if (!isAuthenticated) {
+//       return <Navigate to="/login" replace />;
+//     }
+//     return <Outlet />;
+//   };
+
+//   interface PublicRouteProps {
+//     element: ReactElement;
+//   }
+
+
+//   //SI QUIERE INGRESAR AL LOGIN ALGUIEN CON SESION ABIERTA REDIRECCIONA A LA PAGINA DE NAVIGATE
+//   const PublicRoute: React.FC<PublicRouteProps> = ({ element }) => {
+//     return isAuthenticated ? <Navigate to="/students" replace /> : element;
+//   };
+
+
+//   // const PublicRoute: React.FC<PublicRouteProps> = ({ element }) => {
+//   //   const userInfo = localStorage.getItem("userInfo");
+//   //   if (userInfo) {
+//   //     const { roleName } = JSON.parse(userInfo);
+//   //     if (roleName === "Administrador") {
+//   //       return <Navigate to="/adminDashboard" replace />;
+//   //     } else if (roleName === "Profesor") {
+//   //       return <Navigate to="/professorDashboard" replace />;
+//   //     }
+//   //   }
+    
+//   //   return isAuthenticated ? <Navigate to="/students" replace /> : element;
+//   // };
+  
+
+//   const logout = () => {
+//     localStorage.removeItem("token");
+//     setIsAuthenticated(false);
+//   };
+
+//   // const login = () => {
+//   //   setIsAuthenticated(true);
+//   //   return <Navigate to="/students" replace />;
+//   // };
+
+//   const login = () => {
+//     const userInfo = localStorage.getItem("userInfo");
+//     if (userInfo) {
+//       const { roleName } = JSON.parse(userInfo);
+//       setIsAuthenticated(true);
+//       // setRoleName(roleName); // ¡Este setRoleName estaba causando el error!
+//       if (roleName === "Administrador") {
+//         return <Navigate to="/adminDashboard" replace />;
+//       } else if (roleName === "Profesor") {
+//         return <Navigate to="/professorDashboard" replace />;
+//       }
+//     }
+//   };
+  
+  
+//   const notasEjemplo = [
+//     { materia: "Matemáticas", nota: 9.0 },
+//     { materia: "Ciencias", nota: 8.5 },
+//     { materia: "Historia", nota: 8.0 },
+//     { materia: "Lenguaje", nota: 7.0 },
+//     { materia: "Educacion", nota: 8.0 },
+//   ];
+
+//   return (
+//     <Router>
+//       {isAuthenticated && <Navbar logout={logout} />}
+//       <div className="col py-3">
+//         <Toaster
+//           toastOptions={{
+//             duration: 3000,
+//             style: {
+//               background: "#363636",
+//               color: "#fff",
+//             },
+//           }}
+//           position="top-center"
+//         />
+//         <Routes>
+//           {/* Public routes */}
+//           <Route
+//             path="/login"
+//             element={<PublicRoute element={<LoginForm login={login} />} />}
+//           />
+//           <Route
+//             path="/"
+//             element={<PublicRoute element={<LoginForm login={login} />} />}
+//           />
+          
+//           {/* Protected routes */}
+//           <Route element={<AuthGuard />}>
+//             <Route path="/students" element={<Students />} />
+//             <Route path="/rols" element={<Rols />} />
+//             <Route path="/parents" element={<Parents />} />
+//             <Route path="/parentAssociations" element={<ParentAssociations />} />
+//             <Route path="/register-student" element={<Register />} />
+//             <Route path="/grades" element={<GradeList />} />
+//             <Route path="/grades/add" element={<GradeForm />} />
+//             <Route path="/students/grade/:id" element={<StudentsGrade />} />
+//             <Route path="/register-subjects" element={<Subject />} />
+//             <Route path="/register-teachers" element={<Teacher />} />
+//             <Route path="/subjects" element={<Subject />} />
+//             <Route path="/grades/edit/:id" element={<GradeEdit />} />
+//             <Route path="/attendances" element={<Attendance />} />
+//             <Route path="/adminDashboard" element={<AdminDashboard />} />
+//             <Route path="/professorDashboard" element={<ProfessorDashboard />} />
+//             <Route
+//               path="/notes"
+//               element={
+//                 <ReportGrade
+//                   estudiante="Irvin Paredes"
+//                   grado="7-A"
+//                   key={"ds"}
+//                   notas={notasEjemplo}
+//                 />
+//               }
+//             />
+//             <Route path="/observations" element={<Observation />} />
+//             <Route path="/migrades" element={<MiGrade />} />
+//           </Route>
+//         </Routes>
+//       </div>
+//     </Router>
+//   );
+// };
+
+// export default App;
+
+
 import React, { useEffect, useState, ReactElement } from "react";
 import {
   BrowserRouter as Router,
@@ -150,10 +320,13 @@ import { GradeForm, GradeList, GradeEdit } from "./views/Grade";
 import { StudentsGrade } from "./views/StudentsGrade";
 import { AdminDashboard, ProfessorDashboard } from "./views/Dashboard";
 
+interface PublicRouteProps {
+  element: ReactElement;
+}
+
 const App = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(
-    !!localStorage.getItem("token")
-  );
+  const [isAuthenticated, setIsAuthenticated] = useState(!!localStorage.getItem("token"));
+  const [roleName, setRoleName] = useState("");
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -162,6 +335,14 @@ const App = () => {
     }
   }, []);
 
+  useEffect(() => {
+    const userInfo = localStorage.getItem("userInfo");
+    if (userInfo) {
+      const { roleName } = JSON.parse(userInfo);
+      setRoleName(roleName);
+    }
+  }, [isAuthenticated]);
+
   const AuthGuard = () => {
     if (!isAuthenticated) {
       return <Navigate to="/login" replace />;
@@ -169,22 +350,40 @@ const App = () => {
     return <Outlet />;
   };
 
-  interface PublicRouteProps {
-    element: ReactElement;
-  }
 
+  //se verifica si es admin o profesor y redirige a la pagina adecuadada, si no es ninguno envia al login
   const PublicRoute: React.FC<PublicRouteProps> = ({ element }) => {
-    return isAuthenticated ? <Navigate to="/students" replace /> : element;
+    if (isAuthenticated) {
+      if (roleName === "Administrador") {
+        return <Navigate to="/adminDashboard" replace />;
+      } else if (roleName === "Profesor") {
+        return <Navigate to="/professorDashboard" replace />;
+      } else {
+        return <Navigate to="/login" replace />;
+      }
+    } else {
+      return element;
+    }
   };
 
   const logout = () => {
     localStorage.removeItem("token");
     setIsAuthenticated(false);
+    setRoleName("");
   };
 
   const login = () => {
-    setIsAuthenticated(true);
-    return <Navigate to="/students" replace />;
+    const userInfo = localStorage.getItem("userInfo");
+    if (userInfo) {
+      const { roleName } = JSON.parse(userInfo);
+      setIsAuthenticated(true);
+      setRoleName(roleName);
+      if (roleName === "Administrador") {
+        return <Navigate to="/adminDashboard" replace />;
+      } else if (roleName === "Profesor") {
+        return <Navigate to="/professorDashboard" replace />;
+      }
+    }
   };
 
   const notasEjemplo = [
@@ -258,107 +457,3 @@ const App = () => {
 };
 
 export default App;
-
-
-// import React, { useEffect, useState, ReactElement } from "react";
-// import {
-//   BrowserRouter as Router,
-//   Route,
-//   Routes,
-//   Navigate,
-//   Outlet,
-// } from "react-router-dom";
-// import { Navbar } from "./components/Navbar";
-// import LoginForm from "./views/Login/Login";
-// import { Toaster } from "react-hot-toast";
-// import { AdminDashboard, ProfessorDashboard } from "./views/Dashboard";
-
-// const App = () => {
-//   const [isAuthenticated, setIsAuthenticated] = useState(
-//     !!localStorage.getItem("token")
-//   );
-//   const [roleName, setRoleName] = useState<string | null>(null);
-
-//   useEffect(() => {
-//     const token = localStorage.getItem("token");
-//     if (token) {
-//       setIsAuthenticated(true);
-//       const userInfo = localStorage.getItem("userInfo");
-//       if (userInfo) {
-//         const { roleName } = JSON.parse(userInfo);
-//         setRoleName(roleName);
-//       }
-//     }
-//   }, []);
-
-//   const AuthGuard = () => {
-//     if (!isAuthenticated) {
-//       return <Navigate to="/login" replace />;
-//     }
-//     return <Outlet />;
-//   };
-
-//   interface PublicRouteProps {
-//     element: ReactElement;
-//   }
-
-//   const PublicRoute: React.FC<PublicRouteProps> = ({ element }) => {
-//     if (isAuthenticated && roleName) {
-//       if (roleName === "Administrador") {
-//         return <Navigate to="/adminDashboard" replace />;
-//       } else if (roleName === "Profesor") {
-//         return <Navigate to="/professorDashboard" replace />;
-//       }
-//     }
-//     return element;
-//   };
-
-//   const logout = () => {
-//     localStorage.removeItem("token");
-//     localStorage.removeItem("userInfo");
-//     setIsAuthenticated(false);
-//   };
-
-//   const login = () => {
-//     setIsAuthenticated(true);
-//     return <Navigate to="/students" replace />;
-//   };
-
-//   return (
-//     <Router>
-//       {isAuthenticated && <Navbar logout={logout} />}
-//       <div className="col py-3">
-//         <Toaster
-//           toastOptions={{
-//             duration: 3000,
-//             style: {
-//               background: "#363636",
-//               color: "#fff",
-//             },
-//           }}
-//           position="top-center"
-//         />
-//         <Routes>
-//           {/* Public routes */}
-//           <Route
-//             path="/login"
-//             element={<PublicRoute element={<LoginForm login={login} />} />}
-//           />
-//           <Route
-//             path="/"
-//             element={<PublicRoute element={<LoginForm login={login} />} />}
-//           />
-          
-//           {/* Protected routes */}
-//           <Route element={<AuthGuard />}>
-//             {/* Resto de las rutas protegidas */}
-//             <Route path="/adminDashboard" element={<AdminDashboard />} />
-//             <Route path="/professorDashboard" element={<ProfessorDashboard />} />
-//           </Route>
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// };
-
-// export default App;
--- src/views/Login/Login.tsx
+++ src/views/Login/Login.tsx
@@ -1,3 +1,112 @@
+// import React, { useState } from "react";
+// import { Container, Row, Col, Form, Button } from "react-bootstrap";
+// import "./login.css";
+// import AuthService from "../../services/AuthService";
+// import { useNavigate } from "react-router-dom";
+
+// interface LoginProps {
+//   login: () => void;
+// }
+
+// const LoginForm: React.FC<LoginProps> = ({ login }) => {
+//   const navigate = useNavigate();
+//   const [emailOrUsername, setEmailOrUsername] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [error, setError] = useState("");
+
+//   const handleEmailOrUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//     setEmailOrUsername(e.target.value);
+//   };
+
+//   const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//     setPassword(e.target.value);
+//   };
+
+//   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+//     e.preventDefault();
+
+//     try {
+//       const response = await AuthService.loginUser(emailOrUsername, password);
+//       if (response.success && response.data) {
+//         const userInfo = {
+//           token: response.token,
+//           username: response.data.userName,
+//           roleName: response.data.role,
+//           firstName: response.data.firstName,
+//           lastName: response.data.lastName,
+//           id: response.data.id,
+//         };
+
+//         localStorage.setItem("token", response.token ?? "");
+//         localStorage.setItem("userInfo", JSON.stringify(userInfo));
+
+//         login();
+//         navigate("/students");
+//         setEmailOrUsername("");
+//         setPassword("");
+//       } else {
+//         setError(response.message || "An error occurred.");
+//       }
+//     } catch (error) {
+//       setError("Error API.");
+//     }
+//   };
+
+//   return (
+//     <Container fluid>
+//       <Row className="no-gutter">
+//         <Col md={6} className="d-none d-md-flex bg-image"></Col>
+//         <Col md={6} className="bg-img">
+//           <div className="login d-flex align-items-center py-5">
+//             <Container>
+//               <Row>
+//                 <Col lg={10} xl={7} className="mx-auto">
+//                   <h3 className="display-4">Bienvenido!</h3>
+//                   <h4 className="mb-4">Ingresa tus credenciales.</h4>
+//                   {error && <div className="text-danger mb-3">{error}</div>}
+//                   <Form onSubmit={handleSubmit}>
+//                     <Form.Group controlId="formBasicEmailOrUsername" className="mb-3">
+//                       <Form.Label>Correo o Nombre de Usuario</Form.Label>
+//                       <Form.Control
+//                         type="text"
+//                         placeholder="Enter email or username"
+//                         value={emailOrUsername}
+//                         onChange={handleEmailOrUsernameChange}
+//                       />
+//                     </Form.Group>
+//                     <Form.Group controlId="formBasicPassword" className="mb-3">
+//                       <Form.Label>Contraseña</Form.Label>
+//                       <Form.Control
+//                         type="password"
+//                         placeholder="Password"
+//                         value={password}
+//                         onChange={handlePasswordChange}
+//                       />
+//                     </Form.Group>
+//                     <Form.Group controlId="formBasicCheckbox" className="mb-3">
+//                       <Form.Check type="checkbox" label="Recuerdame" />
+//                     </Form.Group>
+//                     <Button
+//                       variant="primary"
+//                       type="submit"
+//                       className="mb-2 rounded-pill shadow-sm"
+//                     >
+//                       Iniciar Sesion
+//                     </Button>
+//                   </Form>
+//                 </Col>
+//               </Row>
+//             </Container>
+//           </div>
+//         </Col>
+//       </Row>
+//     </Container>
+//   );
+// };
+
+// export default LoginForm;
+
+
 import React, { useState } from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import "./login.css";
@@ -27,6 +136,7 @@ const LoginForm: React.FC<LoginProps> = ({ login }) => {
 
     try {
       const response = await AuthService.loginUser(emailOrUsername, password);
+
       if (response.success && response.data) {
         const userInfo = {
           token: response.token,
@@ -40,8 +150,19 @@ const LoginForm: React.FC<LoginProps> = ({ login }) => {
         localStorage.setItem("token", response.token ?? "");
         localStorage.setItem("userInfo", JSON.stringify(userInfo));
 
+        // Redirigir según el rol del usuario
+        if (response.data.role === "Administrador") {
+          navigate("/adminDashboard");
+        } else if (response.data.role === "Profesor") {
+          navigate("/professorDashboard");
+        } else {
+          navigate("/students"); // Por defecto o para otros roles
+        }
+
+        // Llama a la función de login para actualizar el estado global/contexto de la app
         login();
-        navigate("/students");
+        
+        // Limpiar los campos de entrada
         setEmailOrUsername("");
         setPassword("");
       } else {
@@ -106,116 +227,3 @@ const LoginForm: React.FC<LoginProps> = ({ login }) => {
 
 export default LoginForm;
 
-
-// import React, { useState } from "react";
-// import { Container, Row, Col, Form, Button } from "react-bootstrap";
-// import "./login.css";
-// import AuthService from "../../services/AuthService";
-// import { useNavigate } from "react-router-dom";
-
-// interface LoginProps {
-//   login: () => void;
-// }
-
-// const LoginForm: React.FC<LoginProps> = ({ login }) => {
-//   const navigate = useNavigate();
-//   const [emailOrUsername, setEmailOrUsername] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [error, setError] = useState("");
-
-//   const handleEmailOrUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-//     setEmailOrUsername(e.target.value);
-//   };
-
-//   const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-//     setPassword(e.target.value);
-//   };
-
-//   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-//     e.preventDefault();
-
-//     try {
-//       const response = await AuthService.loginUser(emailOrUsername, password);
-//       if (response.success && response.data) {
-//         const userInfo = {
-//           token: response.token,
-//           username: response.data.userName,
-//           roleName: response.data.role,
-//           firstName: response.data.firstName,
-//           lastName: response.data.lastName,
-//           id: response.data.id,
-//         };
-
-//         localStorage.setItem("token", response.token ?? "");
-//         localStorage.setItem("userInfo", JSON.stringify(userInfo));
-
-//         // Redirigir según el rol del usuario SOLO ESTO SE AGREGOE
-//         if (response.data.role === "Administrador") {
-//           navigate("/students");
-//         } else if (response.data.role === "Profesor") {
-//           navigate("/professorDashboard");
-//         }
-
-//         setEmailOrUsername("");
-//         setPassword("");
-//       } else {
-//         setError(response.message || "An error occurred.");
-//       }
-//     } catch (error) {
-//       setError("Error API.");
-//     }
-//   };
-
-//   return (
-//     <Container fluid>
-//       <Row className="no-gutter">
-//         <Col md={6} className="d-none d-md-flex bg-image"></Col>
-//         <Col md={6} className="bg-img">
-//           <div className="login d-flex align-items-center py-5">
-//             <Container>
-//               <Row>
-//                 <Col lg={10} xl={7} className="mx-auto">
-//                   <h3 className="display-4">Bienvenido!</h3>
-//                   <h4 className="mb-4">Ingresa tus credenciales.</h4>
-//                   {error && <div className="text-danger mb-3">{error}</div>}
-//                   <Form onSubmit={handleSubmit}>
-//                     <Form.Group controlId="formBasicEmailOrUsername" className="mb-3">
-//                       <Form.Label>Correo o Nombre de Usuario</Form.Label>
-//                       <Form.Control
-//                         type="text"
-//                         placeholder="Enter email or username"
-//                         value={emailOrUsername}
-//                         onChange={handleEmailOrUsernameChange}
-//                       />
-//                     </Form.Group>
-//                     <Form.Group controlId="formBasicPassword" className="mb-3">
-//                       <Form.Label>Contraseña</Form.Label>
-//                       <Form.Control
-//                         type="password"
-//                         placeholder="Password"
-//                         value={password}
-//                         onChange={handlePasswordChange}
-//                       />
-//                     </Form.Group>
-//                     <Form.Group controlId="formBasicCheckbox" className="mb-3">
-//                       <Form.Check type="checkbox" label="Recuerdame" />
-//                     </Form.Group>
-//                     <Button
-//                       variant="primary"
-//                       type="submit"
-//                       className="mb-2 rounded-pill shadow-sm"
-//                     >
-//                       Iniciar Sesion
-//                     </Button>
-//                   </Form>
-//                 </Col>
-//               </Row>
-//             </Container>
-//           </div>
-//         </Col>
-//       </Row>
-//     </Container>
-//   );
-// };
-
-// export default LoginForm;
