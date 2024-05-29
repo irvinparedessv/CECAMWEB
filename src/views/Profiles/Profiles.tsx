6f93af0c66209c022b803338608f2dfd48bf7140
SaulRivera99 <saulrivera9999@gmail.com>
2024-05-28 19:27:06 -0600

    arreglando vista de profile

--- src/views/Profiles/Profiles.tsx
+++ src/views/Profiles/Profiles.tsx
@@ -114,16 +114,20 @@
 // export default Profiles;
 
 import React, { useEffect, useState } from 'react';
-import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap';
+import Swal from 'sweetalert2';
+import { Container, Row, Col, Card, Form } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { UserInformation } from '../../types/Login';
 import AuthService from '../../services/AuthService';
 import UserService from '../../services/UserService';
+import './profiles.css'; // Importa el archivo de estilos
 
 const Profiles = () => {
   const [userInfo, setUserInfo] = useState<UserInformation | null>(null);
   const [photo, setPhoto] = useState<File | null>(null);
+  const [isHovered, setIsHovered] = useState(false);
+  const [isLoading, setIsLoading] = useState(false); // Estado para controlar la visibilidad del swal2
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -140,30 +144,26 @@ const Profiles = () => {
     fetchUserInfo();
   }, [navigate]);
 
-  const handlePhotoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handlePhotoChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files.length > 0) {
-      setPhoto(e.target.files[0]);
-    }
-  };
+      const selectedPhoto = e.target.files[0];
+      setPhoto(selectedPhoto);
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+      const formData = new FormData();
+      formData.append('photo', selectedPhoto);
 
-    if (!photo) {
-      return;
-    }
+      try {
+        setIsLoading(true); // Mostrar swal2
+        await UserService.uploadPhoto(formData); // Guardar la foto
 
-    const formData = new FormData();
-    formData.append('photo', photo);
-
-    try {
-      await UserService.uploadPhoto(formData); // Guardar la foto
-
-      // Actualizar los detalles del usuario después de guardar la imagen
-      const updatedUserInfo = await AuthService.getUserDetails();
-      setUserInfo(updatedUserInfo);
-    } catch (error) {
-      console.error('Error al cargar la foto:', error);
+        // Actualizar los detalles del usuario después de guardar la imagen
+        const updatedUserInfo = await AuthService.getUserDetails();
+        setUserInfo(updatedUserInfo);
+      } catch (error) {
+        console.error('Error al cargar la foto:', error);
+      } finally {
+        setIsLoading(false); // Ocultar swal2
+      }
     }
   };
 
@@ -171,57 +171,61 @@ const Profiles = () => {
     return null;
   }
 
-  const { firstName, lastName, userName, rolId, userPhoto, id, email } = userInfo;
+  const { firstName, lastName, userName, rolId, userPhoto, email } = userInfo;
   const DEFAULT_USER_PHOTO_URL = 'http://localhost:8000/storage/userPhoto/default-user-photo.jpg';
   const userPhotoUrl = userPhoto ? `http://localhost:8000/storage/${userPhoto}` : DEFAULT_USER_PHOTO_URL;
 
   return (
-    <Container fluid className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
-      <Row>
-        <Col>
-          <Card style={{ width: '18rem', textAlign: 'center' }}>
-            <Card.Img
-              variant="top"
-              src={userPhotoUrl}
-              alt={`${firstName} ${lastName}`}
-              style={{ width: '200px', height: '200px', objectFit: 'cover', borderRadius: '50%', margin: 'auto', marginTop: '20px' }}
-            />
-
+    <Container className="py-5">
+      <Row className="justify-content-center">
+        <Col lg={6}>
+          <Card>
+            <Card.Header className="text-center">
+              <h2>Perfil de Usuario</h2>
+            </Card.Header>
             <Card.Body>
-              <Card.Title>{`${firstName} ${lastName}`}</Card.Title>
-              <Card.Text>
-                <strong>First Name:</strong> {firstName}
-              </Card.Text>
-              <Card.Text>
-                <strong>Last Name:</strong> {lastName}
-              </Card.Text>
-              <Card.Text>
-                <strong>Username:</strong> {userName}
-              </Card.Text>
-              <Card.Text>
-                <strong>Rol:</strong> {rolId === 5 ? 'Administrador' : (rolId === 3 ? 'Profesor' : 'Otro')}
-              </Card.Text>
-              <Card.Text>
-                <strong>Email:</strong> {email}
-              </Card.Text>
-              <Card.Text>
-                <strong>Photo Path:</strong> {userPhoto}
-              </Card.Text>
-              <Form onSubmit={handleSubmit}>
-                <Form.Group controlId="formPhoto">
-                  <Form.Label>Cambiar Foto de Perfil:</Form.Label>
-                  <Form.Control type="file" onChange={handlePhotoChange} />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                  Guardar Foto
-                </Button>
-              </Form>
+              <div className="text-center mb-3 position-relative" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+                <label htmlFor="photoInput">
+                  <img src={userPhotoUrl} alt={`${firstName} ${lastName}`} className={`rounded-circle img-fluid ${isHovered ? 'blur' : ''}`} />
+                  {isHovered && <p className="change-photo-message">Cambiar Foto</p>}
+                  <input type="file" id="photoInput" onChange={handlePhotoChange} style={{ display: 'none' }} />
+                </label>
+              </div>
+              <hr />
+              <div className="text-center">
+                <h4 className="user-name">{`${firstName} ${lastName}`}</h4>
+                <p><strong>Nombre de Usuario:</strong> {userName}</p>
+                <p><strong>Email:</strong> {email}</p>
+                <p><strong>Rol:</strong> {rolId === 5 ? 'Administrador' : (rolId === 3 ? 'Profesor' : 'Otro')}</p>
+              </div>
             </Card.Body>
           </Card>
         </Col>
       </Row>
+      {/* SweetAlert2 para mostrar el estado de carga */}
+      <SwalLoading isLoading={isLoading} />
     </Container>
   );
 };
 
+const SwalLoading = ({ isLoading }: { isLoading: boolean }) => {
+  useEffect(() => {
+    if (isLoading) {
+      Swal.fire({
+        title: 'Cargando...',
+        allowOutsideClick: false,
+        showConfirmButton: false,
+        willOpen: () => {
+          Swal.showLoading();
+        },
+      });
+    } else {
+      Swal.close();
+    }
+  }, [isLoading]);
+
+  return null;
+};
+
 export default Profiles;
+
