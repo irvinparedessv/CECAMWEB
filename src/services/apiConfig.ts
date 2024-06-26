a6e9e06134ee4f40dcb5f214c8d41a9a2be57c72
SaulRivera99 <saulrivera9999@gmail.com>
2024-06-25 20:32:06 -0600

    cambiando archivo apiConfig por el localhost

--- src/services/apiConfig.ts
+++ src/services/apiConfig.ts
@@ -1,3 +1,3 @@
-const API_BASE_URL = "http://192.168.0.3:8000/api"; // Reemplaza esto con la URL base de tu API
+const API_BASE_URL = "http://127.0.0.1:8000/api"; // Reemplaza esto con la URL base de tu API
 
 export default API_BASE_URL;
